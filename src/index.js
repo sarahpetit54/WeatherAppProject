9448b243bc3427e34ae12fa12257ae6334e061df
sarahpetit54 <79552516+sarahpetit54@users.noreply.github.com>
2021-04-11 16:36:09 -0500

    Updated form in HTML and display forecast function in JS

--- src/index.js
+++ src/index.js
@@ -36,37 +36,40 @@ let days = [
 
 return days[day];
 }
-
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="card-group">`;
-
   forecast.forEach(function (forecastDay, index) {
-    if(index < 6) {
-forecastHTML = forecastHTML + `
-                <div class="card text-center h-50" style="width: 4rem;">
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+                <div class="card text-center h-50">
                         <div class="card-body">
                     <h5 class="card-title">
                         <p>${formatDay(forecastDay.dt)}</p>
                     </h5>
                     <p class="card-text">Sunny
                         <br />
-                        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png">
+                        <img src="http://openweathermap.org/img/wn/${
+                          forecastDay.weather[0].icon
+                        }@2x.png">
                         <br />
-                        <span class="maxTemp">${Math.round(forecastDay.temp.max)}°</span>
-                        <span class="minTemp">${Math.round(forecastDay.temp.min)}°</span>
+                        <span class="maxTemp">${Math.round(
+                          forecastDay.temp.max
+                        )}°</span>
+                        <span class="minTemp">${Math.round(
+                          forecastDay.temp.min
+                        )}°</span>
                     </p>
                     </div>
-                    </div>
                 </div>`;
-}}
-)
-forecastHTML = forecastHTML + `</div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
 function getForecast(coordinates) {
   let apiKey = "2bc05ebef1d18dc15fe697066bb20bd0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
